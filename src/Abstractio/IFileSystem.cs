c6a3f23b7dc2190cd149b526ff377ab41e1c12de
Jose Felix <jose.felix@constant.com.br>
2016-04-24 15:41:18 -0300

    Initial Commit

---
+++ src/Abstractio/IFileSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Abstractio
+{
+    public interface IFileSystem
+    {
+        IEnumerable<string> ListFiles(string path);
+        IEnumerable<string> ListFolders(string path);
+        void CreateFolder(string path);
+        void DeleteFolder(string path);
+        void SaveFile(string path, Stream content);
+        void DeleteFile(string path);
+        Stream ReadFile(string path);
+        IEnumerable<string> SearchFiles(string pattern);
+        IEnumerable<string> SearchFolders(string pattern);
+        bool FileExists(string path);
+        bool FolderExists(string path);
+    }
+}
